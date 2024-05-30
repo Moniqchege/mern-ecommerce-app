e88908bd2dc29ae039ffb9352c766fc8c293fb26
Moniqchege <quichege33@gmail.com>
2024-05-30 16:46:48 +0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      Mern App
-    </div>
+    <>
+     <Header />
+     <Outlet />
+     <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Header = () => {
+  return (
+    <header className='h-16'>
+      <div c
