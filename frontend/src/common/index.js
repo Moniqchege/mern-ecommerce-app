cf5698029ecadd05caa23c9dda9355bfa660f346
Moniqchege <quichege33@gmail.com>
2024-06-01 17:09:36 +0300

    commit

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -13,10 +13,18 @@ const SummaryApi = {
     url: `${backendDomin}/api/user-details`,
     method: "get"
   },
-  logout_user : {
-    url : `${backendDomin}/api/userLogout`,
-    method : 'get'
-  }
+  logout_user: {
+    url: `${backendDomin}/api/userLogout`,
+    method: 'get'
+  },
+  allUser: {
+    url: `${backendDomin}/api/all-user`,
+    method: 'get'
+  },
+  updateUser: {
+    url: `${backendDomin}/api/update-user`,
+    method: "post"
+  },
 }
 
 export default SummaryApi
