4d902632188c4ab6c4da90e6fcde9533b1935ed7
Moniqchege <quichege33@gmail.com>
2024-06-17 11:43:46 +0300

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,34 @@
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-require('dotenv').config()
-const connectDB = require('./config/db')
-const router = require('./routes')
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const router = require('./routes');
 
-
-const app = express()
+const app = express();
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
-app.use(express.json())
-app.use(cookieParser())
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api",router)
+app.use("/api", router);
 
-const PORT = 8080 || process.env.PORT
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+// Anything that doesn't match the api routes should serve the React frontend app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("connnect to DB")
-        console.log("Server is running "+PORT)
-    })
-})
+const PORT = process.env.PORT || 8080;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Connected to DB");
+        console.log("Server is running on port " + PORT);
+    });
+});
