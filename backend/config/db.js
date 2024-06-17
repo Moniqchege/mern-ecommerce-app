6167b211e474faf94a774ee6e8f2b45809b55f9b
Moniqchege <quichege33@gmail.com>
2024-06-17 12:08:58 +0300

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,21 @@
-const mongoose = require("mongoose")
+// backend/config/db.js
+const mongoose = require('mongoose');
 
-
-async function connectDB(){
-    try{
-        await mongoose.connect(process.env.MONGODB_URI)
-    }catch(err){
-        console.log(err)
+const connectDB = async () => {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+        throw new Error('MONGO_URI environment variable not defined');
     }
-}
+    try {
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables
 const connectDB = require('./config/db');
 const router = require('./routes');
 
 const app = express();
+
+// Middleware
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
@@ -14,21 +16,24 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", router);
+// API Routes
+app.use('/api', router);
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// Anything that doesn't match the api routes should serve the React frontend app
+// Catch-all route to serve the React app for any route not handled by the API
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
+// Port configuration
 const PORT = process.env.PORT || 8080;
 
+// Connect to the database and start the server
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log("Connected to DB");
-        console.log("Server is running on port " + PORT);
+        console.log('Connected to DB');
+        console.log('Server is running on port ' + PORT);
     });
 });
